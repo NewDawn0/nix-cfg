290c882334dc56d6ba2540d85c598be4c1035fe1
NewDawn0 <92283105+NewDawn0@users.noreply.github.com>
2023-10-07 19:05:20 +0200

    add darwin-main

---
+++ darwin/default.nix
@@ -0,0 +1,39 @@
+{ pkgs, ... }: {
+  services.nix-daemon.enable = true;
+  imports = [ ../shared/nix.nix ./system.nix ./brew.nix ];
+  environment = import ../shared/env.nix { inherit pkgs; };
+
+  fonts = import ../shared/fonts.nix { inherit pkgs; };
+  services = {
+    emacs.enable = true;
+    skhd = import ./skhd.nix { inherit pkgs; }; # Currently broken :(
+    yabai = import ./yabai.nix { inherit pkgs; };
+    khd = import ./khd.nix { inherit pkgs; };
+    # spacebar = import ./spacebar.nix { inherit pkgs; };
+  };
+  programs = {
+    # Shells
+    zsh = {
+      enable = true;
+      enableCompletion = true;
+      enableBashCompletion = false;
+      enableFzfCompletion = false;
+      enableFzfHistory = true;
+      enableSyntaxHighlighting = true;
+    };
+    bash = {
+      enable = true;
+      enableCompletion = true;
+    };
+    # Shell tools
+    tmux = {
+      enable = true;
+      enableMouse = true;
+    };
+    # Docs
+    man.enable = true;
+    info.enable = true;
+    # Nix
+    nix-index.enable = true;
+  };
+}
