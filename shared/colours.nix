9f2099ce488dbe6bb8017f247a512350e2f31805
NewDawn0 <92283105+NewDawn0@users.noreply.github.com>
2023-10-07 19:09:18 +0200

    add colourschemes

---
+++ shared/colours.nix
@@ -0,0 +1,72 @@
+{
+  themes = {
+    everblush = {
+      primary = {
+        background = "#141b1e";
+        foreground = "#dadada";
+      };
+      normal = {
+        black = "#232a2d";
+        red = "#e57474";
+        green = "#8ccf7e";
+        yellow = "#e5c76b";
+        blue = "#67b0e8";
+        magenta = "#c47fd5";
+        cyan = "#6cbfbf";
+        white = "#b3b9b8";
+      };
+      bright = {
+        black = "#2d3437";
+        red = "#ef7e7e";
+        green = "#96d988";
+        yellow = "#f4d67a";
+        blue = "#71baf2";
+        magenta = "#ce89df";
+        cyan = "#67cbe7";
+        white = "#bdc3c2";
+      };
+      extra = {
+        pink = "#ee9cdd";
+        blueExtra = "#5aa3db";
+        cyan = "#6cbfbf";
+        teal = "#9bdead";
+        purple = "#c47fd5";
+        orange = "#fcb163";
+      };
+    };
+    onedark = {
+      primary = {
+        background = "#1e2127";
+        foreground = "#abb2bf";
+      };
+      normal = {
+        black = "#1e2127";
+        red = "#e06c75";
+        green = "#98c379";
+        yellow = "#d19a66";
+        blue = "#61afef";
+        magenta = "#c678dd";
+        cyan = "#56b6c2";
+        white = "#828791";
+      };
+      bright = {
+        black = "#5c6370";
+        red = "#e06c75";
+        green = "#98c379";
+        yellow = "#d19a66";
+        blue = "#61afef";
+        magenta = "#c678dd";
+        cyan = "#56b6c2";
+        white = "#e6efff";
+      };
+      extra = {
+        pink = "#ff75a0";
+        blueExtra = "#81a1c1";
+        cyan = "#a3b8ef";
+        teal = "#519aba";
+        purple = "#de98fd";
+        orange = "#fca2aa";
+      };
+    };
+  };
+}
