2341bae7e2ca77d3f45f66fbedcf095400a5c398
NewDawn0 <92283105+NewDawn0@users.noreply.github.com>
2023-10-07 19:07:10 +0200

    add darwin brew setup

---
+++ darwin/brew.nix
@@ -0,0 +1,20 @@
+{
+  homebrew = {
+    enable = true;
+    global.autoUpdate = false;
+    taps = [{
+      name = "lencx/chatgp";
+      clone_target = "https://github.com/lencx/ChatGPT.git";
+    }];
+    onActivation = {
+      autoUpdate = true;
+      cleanup = "zap";
+      upgrade = true;
+    };
+    masApps = {
+      Xcode = 497799835;
+      Shazam = 897118787;
+    };
+    casks = import ./casks.nix;
+  };
+}
---
+++ darwin/casks.nix
@@ -0,0 +1,31 @@
+[
+  # Communication Tools
+  "microsoft-teams"
+  "microsoft-excel"
+  "microsoft-powerpoint"
+  "microsoft-outlook"
+  "microsoft-word"
+  "whatsapp"
+
+  # Utility Tools
+  "appcleaner"
+  "balenaetcher"
+  "raycast"
+
+  # Entertainment Tools
+  "blender"
+  "blockbench"
+  "iina"
+  "prismlauncher"
+  "vlc"
+
+  # Productivity Tools
+  "github"
+  "languagetool"
+  "chatgpt"
+  "obsidian"
+
+  # Browsers
+  "firefox"
+  # "orion"
+]
