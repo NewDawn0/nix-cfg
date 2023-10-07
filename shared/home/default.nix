7b2a878e2e6d97813dec4ea5ef25d1d741ff3c8d
NewDawn0 <92283105+NewDawn0@users.noreply.github.com>
2023-10-07 19:12:07 +0200

    add home-manager main

---
+++ shared/home/default.nix
@@ -0,0 +1,20 @@
+{ pkgs, ... }:
+let
+  conf = import ../conf.nix;
+  inherit (pkgs) stdenv;
+in {
+  useGlobalPkgs = true;
+  users.${conf.user} = { pkgs, ... }: {
+    home = {
+      stateVersion = "23.05";
+      packages = import
+        (if stdenv.isDarwin then ../../darwin/pkgs.nix else ../pkgs.nix) {
+          inherit pkgs;
+        };
+      enableNixpkgsReleaseCheck = false;
+    };
+    programs = { };
+    imports = [ ./zsh.nix ./starship.nix ./tmux.nix ./git.nix ];
+    # imports = [ ./zsh.nix ./starship.nix ./tmux.nix ./git.nix ./alacritty.nix ];
+  };
+}
