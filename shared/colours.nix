23267483dba43a01b809f3f73530cd0f3abecc0a
NewDawn0 <92283105+NewDawn0@users.noreply.github.com>
2023-10-08 13:50:59 +0200

    add nord and catpuccin

--- shared/colours.nix
+++ shared/colours.nix
@@ -26,12 +26,83 @@
         white = "#bdc3c2";
       };
       extra = {
-        pink = "#ee9cdd";
-        blueExtra = "#5aa3db";
-        cyan = "#6cbfbf";
-        teal = "#9bdead";
-        purple = "#c47fd5";
-        orange = "#fcb163";
+        grey = "#50575a"; # Light grey
+        pink = "#ee9cdd"; # pink
+        blueExtra = "#5aa3db"; # Nord blue
+        cyan = "#6cbfbf"; # cyan
+        teal = "#9bdead"; # teal
+        purple = "#c47fd5"; # purple
+        orange = "#fcb163"; # orange
+      };
+    };
+    nord = {
+      primary = {
+        background = "#2e3440";
+        foreground = "#d8dee9";
+      };
+      normal = {
+        black = "#3b4252";
+        red = "#bf616a";
+        green = "#a3be8c";
+        yellow = "#ebcb8b";
+        blue = "#81a1c1";
+        magenta = "#b48ead";
+        cyan = "#88c0d0";
+        white = "#e5e9f0";
+      };
+      bright = {
+        black = "#4c566a";
+        red = "#bf616a";
+        green = "#a3be8c";
+        yellow = "#ebcb8b";
+        blue = "#81a1c1";
+        magenta = "#b48ead";
+        cyan = "#8fbcbb";
+        white = "#eceff4";
+      };
+      extra = {
+        grey = "#646a76";
+        pink = "#d57780";
+        blueExtra = "#81a1c1";
+        cyan = "#9aafe6";
+        teal = "#6484a4";
+        purple = "#b48ead";
+        orange = "#e39a83";
+      };
+    };
+    catpuccin = {
+      primary = {
+        background = "#1e1e2e";
+        foreground = "#cdd6f4";
+      };
+      normal = {
+        black = "#45475A";
+        red = "#F38BA8";
+        green = "#A6E3A1";
+        yellow = "#F9E2AF";
+        blue = "#89B4FA";
+        magenta = "#F5C2E7";
+        cyan = "#94E2D5";
+        white = "#BAC2DE";
+      };
+      bright = {
+        black = "#585B70";
+        red = "#F38BA8";
+        green = "#A6E3A1";
+        yellow = "#F9E2AF";
+        blue = "#89B4FA";
+        magenta = "#F5C2E7";
+        cyan = "#94E2D5";
+        white = "#A6ADC8";
+      };
+      extra = {
+        grey = "#605f6f";
+        pink = "#f5c2e7";
+        blueExtra = "#8bc2f0";
+        cyan = "#89dceb";
+        teal = "#b5e8e0";
+        purple = "#d0a9e5";
+        orange = "#f8bd96";
       };
     };
     onedark = {
@@ -60,6 +131,7 @@
         white = "#e6efff";
       };
       extra = {
+        grey = "#6f737b";
         pink = "#ff75a0";
         blueExtra = "#81a1c1";
         cyan = "#a3b8ef";
